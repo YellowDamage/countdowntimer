add38e8c42c9a6d9b386489a9a27ff15c46cacaf
Ryan Euler <reule1@ocdsb.ca>
2019-12-11 19:44:58 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var counter=0;
+var timeleft =60;
+
+function convertSeconds(s){
+var min = floor(s/60);
+var sec = s % 60;
+  return nf(min,2) + ':' + nf(sec,2);
+}
+
+function setup() {
+  noCanvas();
+
+var params = getURLParams();
+  if(params.minute)
+  {
+var min = params.minute;
+timeleft = min*60;
+  }
+  
+var timer = select('#timer');
+timer.html(convertSeconds(timeleft - counter));
+  
+  
+  function timeIt(){
+    counter++
+    timer.html(convertSeconds(timeleft - counter));
+  }
+  setInterval(timeIt, 1000);
+}
